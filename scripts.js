d07b11ce83d4f3fc9419b72ca51339c363918bc2
Gambit90s <44451389+Gambit90s@users.noreply.github.com>
2019-02-13 08:43:05 -0700

    First Update

---
+++ scripts.js
@@ -0,0 +1,17 @@
+var body = document.body;
+
+var theSuspect = ['Ronnie', 'Bobbie', 'Ricki', 'Mike', 'Brosef'];
+var inThe = ['Bedroom','Bathroom', 'Hallway','Kitchen','Living Room','Backyard','Basement','Frontyard','Laundry Room','Paris,TX']
+var withThe = ['Gun','Knife', 'Bat','Wrench','Lamp','Chair','Book','Remote Control','Dog Bone','Spoon','Pimp Slap','Iphone', 'Keyboard','Surface Pro','MacBook Pro','Italian Mineral Water','Bad Intentions','Kindness','Motivation','Toe Nail']
+
+$(document).ready(function(){
+    for ( var i =0; i <= 101; i++){
+        theText += lines[i];
+    }  // my idea of a for loop
+
+    function randomAccusation(){
+
+    }
+})
+
+alert(theSuspect + "  in the " + inThe + " with a " + withThe + " Arrest Them Officer! ");
